59ffbf93cec6283c635e152840322bb5a98feb99
Vital A Pi <code@vp.ag>
2024-02-11 21:55:47 +0300

    * reports merging feature -- WARN CLI changed for input files; * alerts exclusion parametrized

--- src/zreprt/__main__.py
+++ src/zreprt/__main__.py
@@ -2,10 +2,19 @@
 
 import argparse
 import sys
+from attrs import evolve
 from io import TextIOWrapper
 from pathlib import Path
+from itertools import chain, groupby
 
 from . import ZapReport
+
+
+DEFAULT_ALERTS_EXCLUDED = (
+    10096,  # Timestamp Disclosure
+    10027,  # Information Disclosure - Suspicious Comments
+    10109,  # Modern Web Application
+)
 
 
 def main():
@@ -17,9 +26,10 @@ def main():
         usage='{ %(prog)s | python -m %(prog)s } [options]',
     )
     parser.add_argument(
-        '-i', '--in_file',
+        'in_file',
+        nargs='*',
         type=argparse.FileType('r'),
-        default=sys.stdin,
+        default=[sys.stdin,],
         help='Input file to parse as ZAP(-like) report, defaults to `-` (STDIN data).'
     )
     parser.add_argument(
@@ -31,44 +41,65 @@ def main():
              ' and to "<filename>-m.<ext>" when "<filename>.<ext>" specified as input.'
     )
     parser.add_argument(
+        '-x',
+        action='append',
+        default=list(),
+        help="Exclude alert(s) by their ZAP's `pluginid`, can be specified multiple times."
+             ' Ref: <https://www.zaproxy.org/docs/alerts/>.'
+             f' Use of this option overrides the default {DEFAULT_ALERTS_EXCLUDED}.'
+    )
+    parser.add_argument(
         '-z', '--zap-original-format', '--zap_original_format',
         action='store_true',
         help='Use ZAP original JSON output. Defaults to False, causing ZAP-like output.',
     )
     args = parser.parse_args()
 
-    zr = ZapReport.from_json_file(args.in_file)
+    zrs = list()
+    for input_file in args.in_file:
+        zr = ZapReport.from_json_file(input_file)
 
-    while len(zr.site) > 1:
-        _ = zr.site.pop(0)
+        while len(zr.site) > 1:
+            _ = zr.site.pop(0)
 
-    for a in zr.site[0].alerts:
-        for i in range(len(a.instances) - 1):
-            a.instances[i].request_header = ''
-            a.instances[i].request_body = ''
-            a.instances[i].response_header = ''
-            a.instances[i].response_body = ''
+        # Exclude some alerts
+        zr.site[0].alerts = list(filter(
+            lambda a: int(a.pluginid) not in (args.x or DEFAULT_ALERTS_EXCLUDED),
+            zr.site[0].alerts
+        ))
 
-    # Exclude some alerts
-    # TODO: Parametrize this
-    zr.site[0].alerts = list(filter(
-        lambda a: int(a.pluginid) not in (
-            10096,  # Timestamp Disclosure
-            10027,  # Information Disclosure - Suspicious Comments
-        ),
-        zr.site[0].alerts
-    ))
+        zrs.append(zr)
+
+    zr_merged = evolve(
+        zrs[0],
+        **({'program_name': '(combo)', 'version': ''} if len(zrs) > 1 else {}),
+        site=[evolve(
+            zrs[0].site[0],
+            alerts=list(),
+        ),]
+    )
+    kf = lambda a: (-int(a.riskcode), a.pluginid, a.alert, a.name, a.otherinfo)
+    for _gk, agrp in groupby(sorted((a for zr in zrs for a in zr.site[0].alerts), key=kf), key=kf):
+        agrp = list(agrp)
+        ais = sorted(set(chain.from_iterable(a.instances for a in agrp)))
+        for i in range(len(ais) - 1):
+            ais[i].request_header = ''
+            ais[i].request_body = ''
+            ais[i].response_header = ''
+            ais[i].response_body = ''
+        new_alert = evolve(agrp[0], instances=ais)
+        zr_merged.site[0].alerts.append(new_alert)
 
     output_file = args.out_file
     if output_file is None:
-        if args.in_file.name == '<stdin>':
+        if args.in_file[0].name == '<stdin>':
             output_file = sys.stdout
         else:
-            input_file = Path(args.in_file.name)
-            output_file = input_file.with_stem(f'{input_file.stem}-m')
+            first_input_file = Path(args.in_file[0].name)
+            output_file = first_input_file.with_stem(f'{first_input_file.stem}-m')
 
     with (output_file if isinstance(output_file, TextIOWrapper) else open(output_file, 'w')) as fo:
-        fo.write(zr.json_orig() if args.zap_original_format else zr.json())
+        fo.write(zr_merged.json_orig() if args.zap_original_format else zr_merged.json())
 
 
 if __name__ == '__main__':
