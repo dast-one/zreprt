c0f4d0d5e81baf43ee996690ff03d0b3cf2bebdb
Vital A Pi <code@vp.ag>
2024-02-17 22:35:16 +0300

    `cattrs.preconf.json` already produces ISO 8601 strings

--- src/zreprt/zreprt.py
+++ src/zreprt/zreprt.py
@@ -32,7 +32,7 @@ def _clns(s, p=re.compile(r'</?p>')):
 
 
 _zlike_conv = make_converter()
-_zlike_conv.register_unstructure_hook(datetime, lambda dt: dt.isoformat())
+# _zlike_conv.register_unstructure_hook(datetime, lambda dt: dt.isoformat())  # cattrs.preconf.json does this
 _zlike_conv.register_structure_hook(datetime, lambda s, _: ts if (ts := dateutil.parser.parse(s)).tzinfo
                                                               else ts.replace(tzinfo=timezone.utc))
 _zorig_conv = _zlike_conv.copy()
